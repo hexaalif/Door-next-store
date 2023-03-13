b3ffacffdfb65d47be1079a90bc723965f8147d8
hexaalif <hexaalif2020@gmail.com>
2023-03-13 17:57:24 +0600

    navbar added

--- Components/Layout.js
+++ Components/Layout.js
@@ -3,32 +3,11 @@ import React from "react";
 import Header from "./Header";
 
 const Layout = ({ children }) => {
-  // const [nav, setNav] = useState(false);
-  // const [clr, setClr] = useState("transparent");
-  // const [txtclr, setTxtclr] = useState("white");
-
-  // const handleNav = () => {
-  //   setNav(!nav);
-  // };
-
-  // useEffect(() => {
-  //   const changeClr = () => {
-  //     if (window.scrollY >= 90) {
-  //       setClr("#ffffff");
-  //       setTxtclr("#000000");
-  //     } else {
-  //       setClr("transparent");
-  //       setTxtclr("#ffffff");
-  //     }
-  //   };
-  //   window.addEventListener("scroll", changeClr);
-  // }, []);
   return (
     <div>
       <div className="flex min-h-screen flex-col justify-between text-center">
         <header className="">
-          {/* <Header /> */}
-          <h1>Header</h1>
+          <Header />
         </header>
         <main>{children}</main>
         <footer>footer</footer>
