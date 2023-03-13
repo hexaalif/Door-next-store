1b8791275b67fee030ceb3b5bf9cd4c2b1beee20
hexaalif <hexaalif2020@gmail.com>
2023-03-14 00:31:30 +0600

    link in img card head added

--- Components/ProductItem.js
+++ Components/ProductItem.js
@@ -10,14 +10,20 @@ export default function ProductItem({ product }) {
       <div className="card hover:bg-red-300 ease-in duration-300 rounded-lg">
         <div className="block md:flex items-center rounded-lg shadow-xl">
           <div className="w-full md:w-1/2 p-6">
-            <img
-              className="rounded-lg h-72 w-72 mx-auto"
-              src={product.image}
-              alt=""
-            />
+            <Link href={`/product/${product.slug}`}>
+              <img
+                className="rounded-lg h-72 w-72 mx-auto"
+                src={product.image}
+                alt=""
+              />
+            </Link>
           </div>
           <div className="p-6 w-full md:w-1/2 text-center md:text-left">
-            <p className=" text-3xl font-bold text-gray-600">{product.name}</p>
+            <Link href={`/product/${product.slug}`}>
+              <p className=" text-3xl font-bold text-gray-600">
+                {product.name}
+              </p>
+            </Link>
             <p className=" text-sm font-bold text-gray-600">{product.brand}</p>
             <p className="mt-5 text-xl font-bold text-gray-600">
               {product.description}
