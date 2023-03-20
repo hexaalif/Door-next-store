ab3c47b3e4dab063bea31ad1a0da7be3a66dd315
hexaalif <hexaalif2020@gmail.com>
2023-03-20 21:47:44 +0600

    mbl responsive cart

--- pages/cart.js
+++ pages/cart.js
@@ -17,12 +17,12 @@ const Cart = () => {
 
   return (
     <Layout>
-      <div className="m-auto w-[1240px] p-6">
+      <div className="max-w-[1240px] m-auto md:p-6">
         {/* hero content */}
-        <div className="absolute top-0 left-0 right-0 bottom-[625px] bg-gray-400 z-[2]"></div>
-        <div className="text-left text-5xl font-bold text-black my-5">
+        <div className="absolute top-0 left-0 right-0 bottom-[490px] md:bottom-[625px] bg-gray-400 z-[2]"></div>
+        <div className="text-left text-xl md:text-5xl font-bold text-black my-5">
           <h1>Shopping Cart</h1>
-          <div className="flex my-10 text-xl font-light">
+          <div className="flex my-10 text-xs md:text-xl font-light">
             <div>
               <Link href="/">Home</Link> /{" "}
             </div>
@@ -41,12 +41,15 @@ const Cart = () => {
         {/* table */}
         {cartItems.length === 0 ? (
           <div>
-            Cart is empty. <Link href="/">Go shopping</Link>
+            Cart is empty.{" "}
+            <Link href="/" className="underline">
+              Go shopping
+            </Link>
           </div>
         ) : (
-          <div className="grid md:grid-cols-4 md:gap-5">
-            <div className="overflow-x-auto md:col-span-3">
-              <table className="min-w-full ">
+          <div className="grid grid-cols-1 md:grid-cols-4 md:gap-5">
+            <div className="md:overflow-x-auto col-span-1 md:col-span-3">
+              <table className=" md:min-w-full ">
                 <thead className="border-b">
                   <tr>
                     <th className="p-5 text-left">Item</th>
